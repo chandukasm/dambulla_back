b53e1a73656c54c8a533dad9a26360d641abbdd4
chanduka <ashainxsamarasinghe@gmail.com>
2020-10-23 09:46:52 +0530

    appp resolved

--- routes/table.js
+++ routes/table.js
@@ -4,7 +4,7 @@ const Joi = require("joi");
 const multer = require("multer");
 const pool = require("../components/connection");
 
-app.get("/patient", (req, res) => {
+router.get("/patient", (req, res) => {
   const rep = pool.query(
     "create table patient(index serail primary key , f_name,l_name,gender varchar(10) , address varchar(255),id varchar(30),dob timestamp with time zone NOT NULL, tel varchar(20) not null"
   );
