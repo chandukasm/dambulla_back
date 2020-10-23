4c99aef9c3cf37d8428ba8a2b54f19fd95f46ac3
chanduka <ashainxsamarasinghe@gmail.com>
2020-10-23 19:38:51 +0530

    resolving query error

--- routes/Patients.js
+++ routes/Patients.js
@@ -20,8 +20,7 @@ router.get("/all", (req, res) => {
 // add new patient id ,id is not available should generate a new uniquie one
 
 router.post("/", async (req, res, next) => {
-
-  req.body.array.forEach(element => {
+  req.body.array.forEach(async (element) => {
     const scheama = {
       f_name: Joi.string().min(2).required(),
       l_name: Joi.string().min(2).required(),
@@ -31,7 +30,7 @@ router.post("/", async (req, res, next) => {
       id: Joi.string().min(5).required(),
       tel: Joi.string(),
     };
-  
+
     const result = Joi.validate(element, scheama);
     if (result.error) {
       console.log(result.error.details[0].message);
@@ -39,17 +38,16 @@ router.post("/", async (req, res, next) => {
       console.log(result.error.details[0].message);
       return;
     }
-  
+
     const { f_name, l_name, gender, dob, address, id, tel } = element;
     const values = [f_name, l_name, dob, gender, address, id, tel];
     const query = `INSERT INTO patient(f_name, l_name, dob, gender,address,id,tel)VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING *`;
-  
+
     const result1 = await pool.query(query, values);
     res.status(200).send(result1.rows);
     console.log(result1.rows);
   });
- 
- 
+
   // no need to add try catchs because express async errors monkypatchs the middleware
 });
 
