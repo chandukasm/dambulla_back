06b798286f07f043296f989c8eb1cd4f444774b4
chanduka <ashainxsamarasinghe@gmail.com>
2020-10-23 09:54:00 +0530

    crating patient table

--- routes/table.js
+++ routes/table.js
@@ -5,11 +5,15 @@ const multer = require("multer");
 const pool = require("../components/connection");
 
 router.get("/patient", (req, res) => {
-  const rep = pool.query(
-    "create table patient(index serail primary key , f_name,l_name,gender varchar(10) , address varchar(255),id varchar(30),dob timestamp with time zone NOT NULL, tel varchar(20) not null"
-  );
-
-  res.status(200).send("table creted successully");
+  try {
+    const rep = pool.query(
+      "create table if not exists patient(index serail primary key , f_name,l_name,gender varchar(10) , address varchar(255),id varchar(30),dob timestamp with time zone NOT NULL, tel varchar(20) not null"
+    );
+    console.log(rep);
+    res.status(200).send("table creted successully");
+  } catch (error) {
+    error.message;
+  }
 });
 
 module.exports = router;
