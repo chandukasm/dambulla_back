eecde837d3c929c0e60ecb68817474e622ef2051
chanduka <ashainxsamarasinghe@gmail.com>
2020-10-23 08:56:25 +0530

    added database credentials

--- components/connection.js
+++ components/connection.js
@@ -1,13 +1,13 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "base_d",
-  password: "eleos",
-  port: 5432,
-  max: 20,
-});
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "base_d",
+//   password: "eleos",
+//   port: 5432,
+//   max: 20,
+// });
 
 // const pool = new Pool({
 //   user: "nnammzahukkhag",
@@ -19,14 +19,14 @@ const pool = new Pool({
 //   ssl: true,
 // });
 
-// const pool = new Pool({
-//   user: "postgres",
-//   host: "dambulla.cz3wrdwn78xb.us-east-2.rds.amazonaws.com",
-//   database: "dambulla",
-//   password: "base_dambulla",
-//   port: 5432,
-//   max: 20,
-//   ssl: true,
-// });
+const pool = new Pool({
+  user: "pmykppbruuqlhu",
+  host: "ec2-52-87-22-151.compute-1.amazonaws.com",
+  database: "d58mlt74778u0r",
+  password: "421c737d47e8f0fac0d0cf5d40c8ba7a1fb3660902c2ecdaafa602ce501cced7",
+  port: 5432,
+  max: 20,
+  ssl: true,
+});
 
 module.exports = pool;
